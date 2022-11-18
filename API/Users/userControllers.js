84b6fd57463f5b99778564721b8792cc2d07d811
Eleli-Ayub <lesosioayub@gmail.com>
2022-11-18 23:11:42 +0300

    finalized making sign in and sign up api with jwt and bcrypt

--- API/Users/userControllers.js
+++ API/Users/userControllers.js
@@ -1,6 +1,6 @@
 const User = require("../../Models/Usermodel");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
 module.exports.signUp = async (req, res) => {
   const saltRounds = 10;
   const {
@@ -24,8 +24,22 @@ module.exports.signUp = async (req, res) => {
     password: hashedPassword,
   });
   try {
-    await newUser.save();
-    res.json({ msg: "Created succesful" });
+    const user = await newUser.save();
+    let token;
+    try {
+      token = jwt.sign({ email }, process.env.SECRET_KEY, {
+        expiresIn: "3h",
+      });
+    } catch (error) {
+      console.log(error);
+      const err = new Error("Error!!Something went wrong");
+      return next(err);
+    }
+    res.status(200).json({
+      msg: "Sign in succesful",
+      success: true,
+      data: { userId: user.id, email: user.email, token: token },
+    });
   } catch (error) {
     res.status(500).send(error);
     console.log(error);
@@ -41,7 +55,21 @@ module.exports.signIn = async (req, res) => {
     if (!compare) {
       res.json({ msg: "Invalid Credentials" });
     } else {
-      res.json({ msg: "Login succesful" });
+      let token;
+      try {
+        token = jwt.sign({ email, password }, process.env.SECRET_KEY, {
+          expiresIn: "3h",
+        });
+      } catch (error) {
+        console.log(error);
+        const err = new Error("Error!!Something went wrong");
+        return next(err);
+      }
+      res.status(200).json({
+        msg: "Login succesful",
+        success: true,
+        data: { userId: user.id, email: user.email, token: token },
+      });
     }
   }
 };
